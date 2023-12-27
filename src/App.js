100a90897448c6f48c76ee4cac5f6ac413b4fdcf
iznaur-hac <obito20077@gmail.com>
2023-12-27 23:43:02 +0300

    jfh

--- src/App.js
+++ src/App.js
@@ -1,53 +1,8 @@
-import { useState } from "react";
-import ToDo from "./components/ToDo";
-import ToDoForm from "./components/ToDoForm";
+import State from "./components/state";
 
 function App() {
-
-  const [todos, setTodos] = useState([])
-
-  const addTask =(input) =>{
-    if (input) {
-      const newItem = {
-        id: Math.random().toString(36).substr(2, 9),
-        task: input,
-        compelete: false
-      }
-      setTodos([...todos, newItem])
-    }
-  }
-
-  const removeTask = (id) =>{
-    setTodos([
-      ...todos.filter(todo => todo.id !== id)
-    ])
-  }
-
-  const changeTask = (id) => {
-    setTodos([
-      ...todos.map((todo) =>{
-      return  todo.id === id ? {...todo, compelete: !todo.compelete} : {...todo}
-      })
-    ])
-  }
-
   return (
-    <div className="container">
-      <header className="app">
-        <h1>Количество задач №{todos.length}</h1>
-      </header>
-      {todos.map(todo =>{
-        return(
-          <ToDo
-          todo={todo}
-          key={todo.id}
-          removeTask={removeTask}
-          changeTask={changeTask}
-          />
-        )
-      })}
-      <ToDoForm addTask={addTask}/>
-    </div>
+    <State/>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider} from 'react-redux';
+import { createStore } from 'redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const reducer = (state, action) =>{
+    if (action.type === 'PLUS'){
+        return state +1
+    }
+    if (action.type === 'MINUS'){
+        return state -1
+    }
+    return 0
+}
+
+const store = createStore(reducer)
+
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
