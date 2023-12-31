f37e19c19bd11a145d4ff46941fad27fd1b3f945
iznaur-hac <obito20077@gmail.com>
2023-12-31 16:18:44 +0300

    dsddkf

--- src/App.js
+++ src/App.js
@@ -1,8 +1,72 @@
-import State from "./components/state";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const todos = useSelector((state) => state);
+
+  const dispatch = useDispatch();
+
+  const removeBtn = (id) => {
+    dispatch({
+      type: "REMOVE",
+      payload: id,
+    });
+  };
+
+  const handlerChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const checkSwap = (id) => {
+    dispatch({
+      type: "SWAP",
+      payload: id,
+    });
+  };
+
+  const addTask = () => {
+    setInput("");
+    dispatch({
+      type: "ADD",
+      payload: input,
+    });
+  };
   return (
-    <State/>
+    <div className="container">
+      <div className="todos">
+        {todos.map((item, index) => {
+          return (
+            <div className="todo-item" key={index}>
+              <div>
+                <input
+                  type="checkbox"
+                  checked={item.done}
+                  onClick={() => checkSwap(index)}
+                />
+              </div>
+              <div className={item.done ? "stick" : ""}>{item.text}</div>
+              <div>
+                <button className="delete-btn" onClick={() => removeBtn(index)}>
+                  x
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <form onSubmit={handlerSubmit}>
+        <div className="input">
+          <input type="text" value={input} onChange={(e) => handlerChange(e)} />
+          <button onClick={addTask}>Add</button>
+        </div>
+      </form>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider} from 'react-redux';
-import { createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const reducer = (state, action) =>{
-    if (action.type === 'PLUS'){
-        return state +1
-    }
-    if (action.type === 'MINUS'){
-        return state -1
-    }
-    return 0
-}
+const initialState = [
+  {
+    text: "html",
+    done: false,
+  },
+  {
+    text: "css",
+    done: false,
+  },
+  {
+    text: "JScript",
+    done: true,
+  },
+  {
+    text: "REACT",
+    done: false,
+  },
+];
 
-const store = createStore(reducer)
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "REMOVE":
+      return state.filter((item, index) => {
+        if (action.payload === index) {
+          return false;
+        }
+        return true;
+      });
+    case "ADD":
+      if (action.payload) {
+        return (state = [
+          ...state,
+          {
+            text: action.payload,
+            done: false,
+          },
+        ]);
+      } else {
+        return state;
+      }
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
 
 root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
